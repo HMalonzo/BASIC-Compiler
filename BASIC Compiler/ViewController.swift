53b966cbad7767806573b54d0754378b875fe06e
HMalonzo <hmalonzo9273@bg-mmcte-20.d214.org>
2016-05-18 08:08:08 -0500

    lol

--- BASIC Compiler/ViewController.swift
+++ BASIC Compiler/ViewController.swift
@@ -10,16 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var textArea = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        textArea = UILabel(frame: CGRect(x: 0,y: 0,width: 170,height: 170))
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
